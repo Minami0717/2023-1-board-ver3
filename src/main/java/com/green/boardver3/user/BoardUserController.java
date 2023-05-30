d17d7693ae2a54a93ce7a71f3f8a67ae5a60c407
남규진 <dft2007@naver.com>
2023-05-30 13:13:07 +0900

    비밀번호 암호화

--- src/main/java/com/green/boardver3/user/BoardUserController.java
+++ src/main/java/com/green/boardver3/user/BoardUserController.java
@@ -1,6 +1,7 @@
 package com.green.boardver3.user;
 
 import com.green.boardver3.user.model.BoardUserInsDto;
+import com.green.boardver3.utils.CommonUtils;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,22 +13,27 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping
 public class BoardUserController {
     private final BoardUserService service;
+    private final CommonUtils commonUtils;
 
     @Autowired
-    public BoardUserController(BoardUserService service){
+    public BoardUserController(BoardUserService service, CommonUtils commonUtils){
         this.service=service;
+        this.commonUtils = commonUtils;
     }
 
     @PostMapping("/user")
     @Operation(summary = "회원가입",description = ""+
-    "uid: [20] 회원아이디,<br>"+
-    "upw: [100] 회원 비밀번호,<br>"+
-    "nm: [30] 회원명,<br>"+
-    "gender: [1] 성별(M: 남성, F: 여성),<br>"+
-    "addr: [100] 대구시 달서구")
+            "uid: [20] 회원아이디,<br>"+
+            "upw: [100] 회원 비밀번호,<br>"+
+            "nm: [30] 회원명,<br>"+
+            "gender: [1] 성별(M: 남성, F: 여성),<br>"+
+            "addr: [100] 대구시 달서구")
     public int postUser(@RequestBody BoardUserInsDto dto){
+//        String upw = dto.getUpw();
+//        String changeUpw = commonUtils.encodeSha256(upw);
+//        dto.setUpw(changeUpw);
+
+        dto.setUpw(commonUtils.encodeSha256(dto.getUpw()));
         return service.insUser(dto);
     }
-
-
 }

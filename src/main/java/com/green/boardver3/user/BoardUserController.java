720ecddb8ac59e6609f31e62cbf071804a538756
정우진 <wash98@naver.com>
2023-05-30 12:57:36 +0900

    final service 수정

--- src/main/java/com/green/boardver3/user/BoardUserController.java
+++ src/main/java/com/green/boardver3/user/BoardUserController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping
 public class BoardUserController {
-    private BoardUserService service;
+    private final BoardUserService service;
 
     @Autowired
     public BoardUserController(BoardUserService service){
